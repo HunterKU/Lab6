59f990068a6279ab16526d279fdb1ff1ca28efcf
Hunter Kelley <hunterkelleyelec@gmail.com>
2021-03-11 23:52:29 -0600

    Lab 5 commit

--- Documents/NetBeansProjects/Lab5/src/test/java/AverageArrayTest.java
+++ Documents/NetBeansProjects/Lab5/src/test/java/AverageArrayTest.java
@@ -19,41 +19,41 @@ public class AverageArrayTest {
     @Test
     public void testGetTotal() {
         System.out.println("getTotal");
-        int[] a = null;
-        int expResult = 0;
+        int[] a = {1,2,3,4,5};
+        int expResult = 15;
         int result = AverageArray.getTotal(a);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+
     }
 
     @Test
     public void testGetAverage() {
         System.out.println("getAverage");
-        int[] a = null;
-        double expResult = 0.0;
+        int[] a = {1,2,3,4,5};
+        double expResult = 7.5;
         double result = AverageArray.getAverage(a);
         assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
+        
     }
 
     @Test
     public void testGetHighest() {
         System.out.println("getHighest");
-        int[] a = null;
-        int expResult = 0;
+        int[] a = {1,2,3,4,5};
+        int expResult = 5;
         int result = AverageArray.getHighest(a);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
 
     @Test
     public void testGetLowest() {
         System.out.println("getLowest");
-        int[] a = null;
-        int expResult = 0;
+        int[] a = {1,2,3,4,5};
+        int expResult = 1;
         int result = AverageArray.getLowest(a);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
     
 }
